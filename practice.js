b98f4214e0371d8ed7a6b7c4ad6062bdaa76dc7d
mohitx1 <mohitrockx3@gmail.com>
2023-08-19 15:56:04 +0530

    Oops ES5 and ES6

--- practice.js
+++ practice.js
@@ -88,3 +88,62 @@ console.log(color)
     default:
         console.log('color is not red or green')
   }
+// //oops
+
+// // Constructor Function
+// function Person(firstName, lastName, dob) {
+//     // Set object properties
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob); // Set to actual date object using Date constructor
+//     // this.getBirthYear = function(){
+//     //   return this.dob.getFullYear();
+//     // }
+//     // this.getFullName = function() {
+//     //   return `${this.firstName} ${this.lastName}`
+//     // }
+//   }
+  
+//   // Get Birth Year
+//   Person.prototype.getBirthYear = function () {
+//     return this.dob.getFullYear();
+//   }
+  
+//   // Get Full Name
+//   Person.prototype.getFullName = function() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+  
+  
+//   // Instantiate an object from the class
+//   const person1 = new Person('John', 'Doe', '7-8-80');
+//   const person2 = new Person('Steve', 'Smith', '8-2-90');
+  
+//   console.log(person2);
+  
+//   // console.log(person1.getBirthYear());
+//   // console.log(person1.getFullName());
+
+
+  // ES6 CLASSES
+class Person {
+    constructor(firstName, lastName, dob) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.dob = new Date(dob);
+    }
+  
+    // Get Birth Year
+    getBirthYear() {
+      return this.dob.getFullYear();
+    }
+  
+    // Get Full Name
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+  
+  const person1 = new Person('John', 'Doe', '7-8-80');
+  console.log(person1.getBirthYear());
+  
