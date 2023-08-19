1fc2c5701c7861224cc0869b12c99f7cd28a76a9
mohitx1 <mohitrockx3@gmail.com>
2023-08-19 14:58:01 +0530

    foreach, map, filter

--- practice.js
+++ practice.js
@@ -9,3 +9,61 @@ console.log(arr)
 console.log(Array.isArray(arr))
 console.log(Array.isArray('arr'))
 console.log(arr.indexOf('hello'))
+
+const people={
+    'name':'Mohit',
+    'email':'mohit@gmail.com',
+    'hobbies':['surfing','playing'],
+    'address':{
+        state:'Jharkhand',
+        district:'ranchi'
+    }
+}
+people.phone=09123456789
+//to add something to object
+
+console.log(people.hobbies.indexOf('surfing'))
+console.log(people.address.district)
+console.log(people.phone)
+
+const peopleJson=JSON.stringify(people);
+console.log(peopleJson)
+
+
+// Array of objects
+const todos = [
+    {
+      id: 1,
+      text: 'Take out trash',
+      isComplete: false
+    },
+    {
+      id: 2,
+      text: 'Dinner with wife',
+      isComplete: false
+    },
+    {
+      id: 3,
+      text: 'Meeting with boss',
+      isComplete: true
+    }
+  ];
+//for each
+
+  todos.forEach((todo)=>{
+    console.log(todo.id)
+  })
+
+  //map
+
+  const todoText=todos.map(function(todos){
+    return todos.text
+  });
+  console.log(todoText)
+
+  //filter
+
+  const filtertodo=todos.filter((todo)=>{
+    return todo.isComplete==true
+  })
+  console.log(filtertodo)
